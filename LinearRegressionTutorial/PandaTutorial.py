d23408c199dc6dee611e73aa5ccd6dce3017be1e
Rs1124 <604347974@qq.com>
2022-07-13 00:01:43 +0800

    panda练习 day2: 增删改查,保存,过滤

--- LinearRegressionTutorial/PandaTutorial.py
+++ LinearRegressionTutorial/PandaTutorial.py
@@ -32,14 +32,37 @@ print("对Type 1和Hp进行联合升序排序后如下:\n")
 print(df.sort_values(['Type 1', 'HP'], ascending=[1, 0]))
 
 # 新增一列数据
+# df['Total'] = df['HP'] + df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed']
+# tip: 这种方法不是永久性的修改,因为这只是将数据存入内存中,如果不保存! 再次运行时,仍然会使用原来的数据集
 
-# tip: 这种方法不是永久性的修改,因为这只是将数据存入内存中,再次运行时,仍然会使用原来的数据集
-df['Total'] = df['HP'] + df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed']
+# 另一种更实用的方法新增一列数据
+df['Total'] = df.iloc[:, 4:10].sum(axis=1)
+# tip: iloc的第二个参数4:10是要求和的范围,并且这个范围是左闭右开的[4,10)
+
 print("对数据集新增一列'Total'数据后如下:\n")
 print(df.head(5))
 
+# 交换某列数据的位置
+cols = list(df.columns.values)
+df = df[cols[0:4] + [cols[-1]] + cols[4:12]]
+# tip: 参数x:y是范围,并且这个范围是左闭右开的[4,10)
+print("将最后一列数据'Total'交换到第5列后如下:\n")
+print(df.head(5))
+
+# 保存新的csv文件
+print("新的csv文件已经保存到当前文件夹下,请自行前往查看,同时记得删除它,因为它只是一个演示样例\n")
+df.to_csv('modified.csv', index=False)
+# tip:有多种函数,以便存储多种文件,比如:to_excel
 
 # 删除一列数据
 df = df.drop(columns=['Total'])
 print("删除'Total'数据后如下:\n")
 print(df.head(5))
+
+# 过滤数据
+new_df = df.loc[(df['Type 1'] == 'Grass') & (df['Type 2'] == 'Poison')]
+# tip: 可以试试把&换成|试试运行结果是什么
+print("Type 1为Grass以及Type 2为Poison的元素如下:\n")
+print(new_df.head(5))
+print("将过滤后的数据另存为一个新的csv文件,记得删除它\n")
+new_df.to_csv('filtered.csv')
