7524b35d27b88be7411aecf11068719d064ea855
Rs1124 <604347974@qq.com>
2022-07-29 23:11:11 +0800

    线性回归练习(多元函数)

---
+++ LinearRegressionTutorial/LinearRegressionMultipleVariables.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import math
+from sklearn import linear_model
+
+""" 导入训练数据集 """
+df = pd.read_csv("E:\Projects\PythonProjects\MachineLearning\LinearRegressionTutorial\DataSet\homeprice_complex_data.csv")
+# 处理数据集中的无效数据(bedroom中的nan):使用bedroom的中位数进行填补
+median_bedrooms = math.floor(df.bedrooms.median())
+df.bedrooms = df.bedrooms.fillna(median_bedrooms)
+
+""" 训练模型 """
+reg = linear_model.LinearRegression()
+reg.fit(df[['area', 'bedrooms', 'age']], df.price)
+# 进行预测
+print(reg.predict([[3000, 3, 40]]))
