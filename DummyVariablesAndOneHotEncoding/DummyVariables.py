b0b7f437a4e6be2ec88eb94795ab7d0d4dc02634
Rs1124 <604347974@qq.com>
2022-08-07 01:20:51 +0800

    学习如何做带有文字型变量的回归模型

---
+++ DummyVariablesAndOneHotEncoding/DummyVariables.py
@@ -0,0 +1,23 @@
+# Topic: 虚拟变量与独热编码
+# Description:
+# Q: 在制作线性回归模型时遇到了文字型变量(Nominal)该怎么办? A: 使用pandas的虚拟变量或者sklearn库的独热编码
+# Q: pandas的虚拟变量是什么? A: pandas会根据原生文字型变量的种类数(n),在原有的数据集上,新增n列数据,用来对应各个文字型变量,用0,1进行赋值
+# Q: sklearn库的独热编码是什么? A: sklearn会将原生文字型变量重新以数字赋值(0, 1, 2 ... n)
+
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+df = pd.read_csv("E:\Projects\PythonProjects\MachineLearning\DummyVariablesAndOneHotEncoding\DataSet\homeprice_nominal_data.csv")
+dummies = pd.get_dummies(df.town)  # 获取虚拟变量
+merged = pd.concat([df, dummies], axis='columns')  # 将虚拟变量与原生数据集合并
+
+# Q: 为什么要将"town"和"west windsor"两列数据删掉
+# A: "town"数据是文字型数据,本来就要将其删掉,对于"west windsor","monroe township","robinsville"这三个虚拟变量则是要随便选取一个将其删除(对最终的预测也不会有影响)
+#    因为多一个变量就意味着会多大量的计算,最终可能会影响模型的准确度
+final = merged.drop(['town', 'west windsor'], axis='columns')  # 获取最后的数据集
+
+model = LinearRegression()
+x = final.drop('price', axis='columns')  # 获取自变量
+y = final.price
+model.fit(x, y)
+print("虚拟变量: ")
+print(model.predict([[2800, 0, 0]]))  # 预测area=2800,town=west windsor的price
---
+++ DummyVariablesAndOneHotEncoding/OneHotEncoding.py
@@ -0,0 +1,28 @@
+# Topic: 虚拟变量与独热编码
+# Description:
+# Q: 在制作线性回归模型时遇到了文字型变量(Nominal)该怎么办? A: 使用pandas的虚拟变量或者sklearn库的独热编码
+# Q: pandas的虚拟变量是什么? A: pandas会根据原生文字型变量的种类数(n),在原有的数据集上,新增n列数据,用来对应各个文字型变量,用0,1进行赋值
+# Q: sklearn库的独热编码是什么? A: sklearn会将原生文字型变量重新以数字赋值(0, 1, 2 ... n),每个种类对应一个数字
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+dfle = pd.read_csv("E:\Projects\PythonProjects\MachineLearning\DummyVariablesAndOneHotEncoding\DataSet\homeprice_nominal_data.csv")
+le = LabelEncoder()
+print(le.fit_transform(dfle.town))  # 文字变量重新赋值
+dfle.town = le.fit_transform(dfle.town)
+x = dfle[['town', 'area']].values
+y = dfle.price
+ohe = ColumnTransformer(
+    [('Ohe', OneHotEncoder(), [0])],
+    remainder='passthrough'
+)
+x = ohe.fit_transform(x)  # 再次生成虚拟变量
+
+x = x[:, 1:]  # 随便删除三列虚拟变量的其中一列,这里删除的第一列,下标为0
+
+model = LinearRegression()
+model.fit(x, y)
+print(model.predict([[1, 0, 2800]]))  # 预测town=robinsville area=2800的price
