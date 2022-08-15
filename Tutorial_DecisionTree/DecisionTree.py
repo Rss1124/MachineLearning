ecc053f54e40a584a633d1476d621b815a5ff5c9
Rs1124 <604347974@qq.com>
2022-08-15 22:52:27 +0800

    决策树教学:只是照抄了代码,体验了一下如何训练模型,其他的知识比如(熵,基尼系数),还需要继续学习

---
+++ Tutorial_DecisionTree/DecisionTree.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+
+df = pd.read_csv("E:\Projects\PythonProjects\MachineLearning\Tutorial_DecisionTree\DataSet\salaries.csv")
+
+""" 处理数据 """
+inputs = df.drop('salary_more_then_100k', axis='columns')
+target = df['salary_more_then_100k']
+# 因为数据中有文字型变量,所以要用虚拟变量进行处理
+le_company = LabelEncoder()
+le_job = LabelEncoder()
+le_degree = LabelEncoder()
+# 在数据集后面手动添加三列虚拟变量
+inputs['company_n'] = le_company.fit_transform(inputs['company'])
+inputs['job_n'] = le_company.fit_transform(inputs['job'])
+inputs['degree_n'] = le_company.fit_transform(inputs['degree'])
+# 删除多余的"文字变量"列
+inputs_n = inputs.drop(['company', 'job', 'degree'], axis='columns')
+
+""" 训练模型 """
+model = tree.DecisionTreeClassifier()
+model.fit(inputs_n, target)
