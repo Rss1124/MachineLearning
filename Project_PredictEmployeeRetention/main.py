35bc762fbc4945840ad2af8155fa34ac5ec4f964
Rs1124 <604347974@qq.com>
2022-08-16 22:47:57 +0800

    练习项目1:预测员工是否离职 新增加了一个功能:绘制出"薪水等级"与"离职"相关的bar图

--- Project_PredictEmployeeRetention/main.py
+++ Project_PredictEmployeeRetention/main.py
@@ -1,21 +1,39 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression  # 因为要预测的数据是"是否离开公司",所以本次采用逻辑回归
 
 df = pd.read_csv("E:\Projects\PythonProjects\MachineLearning\Project_PredictEmployeeRetention\DataSet\HR_comma_sep.csv")
 
 """ 处理数据集 """
-dummies1 = pd.get_dummies(df.sales)  # 获取虚拟变量
-dummies2 = pd.get_dummies(df.salary)  # 获取虚拟变量
-merged = pd.concat([df, dummies1, dummies2], axis='columns')  # 将虚拟变量与原生数据集合并
-final = merged.drop(['sales', 'salary', 'sales', 'low'], axis='columns')  # 获取最后的数据集
+# 使用LabelEncoder处理文字变量
+le_sales = LabelEncoder()
+le_salary = LabelEncoder()
+# 在数据集后面手动添加两列虚拟变量
+df['sales_n'] = le_sales.fit_transform(df['sales'])
+df['salary_n'] = le_sales.fit_transform(df['salary'])
+# 删除多余的"文字变量"列
+final_inputs = df.drop(['left', 'sales', 'salary'], axis='columns')
+target = df['left']
+# 将数据分成"训练数据"和"测试数据"
+x_train, x_test, y_train, y_test = train_test_split(final_inputs, target, test_size=0.1)
 
-x = final[['satisfaction_level', 'last_evaluation', 'number_project', 'average_montly_hours', 'time_spend_company', 'Work_accident',
-        'promotion_last_5years', 'IT', 'RandD', 'accounting', 'hr', 'management', 'marketing', 'product_mng', 'support', 'technical', 'high', 'medium']]
-y = final['left']
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)  # 将数据分成"训练数据"和"测试数据"
+""" 绘制出"薪水等级"与"离职"相关的bar图 """
+# 统计不同薪水的人的离职数据
+left_high = df.loc[(df['salary'] == 'high') & (df['left'] == 1)].count()['salary']  # 薪水为"high"且"离职"的人数总和
+left_medium = df.loc[(df['salary'] == 'medium') & (df['left'] == 1)].count()['salary']  # 薪水为"medium"且"离职"的人数总和
+left_low = df.loc[(df['salary'] == 'low') & (df['left'] == 1)].count()['salary']  # 薪水为"low"且"离职"的人数总和
+# 画图
+plt.figure(figsize=(10, 7))
+values = (left_low, left_medium, left_high)
+bar = plt.bar(np.arange(3), values, width=0.35, color="#87CEFA")
+plt.xlabel('salary_level')
+plt.ylabel('left_count')
+plt.xticks(np.arange(3), ('low', 'medium', 'high'))
+plt.yticks(np.arange(0, 2200, 100))
+plt.show()
 
 """ 制作逻辑回归模型 """
 model = LogisticRegression()
