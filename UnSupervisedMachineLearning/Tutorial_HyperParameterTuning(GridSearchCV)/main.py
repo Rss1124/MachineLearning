e6befffe4bee4da5d5e1572dbf9e00b1d9145870
Rs1124 <604347974@qq.com>
2022-09-05 21:12:16 +0800

    HyperParameterTuning(GridSearchCV):研究不同模型的不同参数对准确度的影响

--- UnSupervisedMachineLearning/Tutorial_HyperParameterTuning(GridSearchCV)/main.py
+++ UnSupervisedMachineLearning/Tutorial_HyperParameterTuning(GridSearchCV)/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, RandomizedSearchCV
 
@@ -46,3 +48,38 @@ rs = RandomizedSearchCV(svm.SVC(gamma='auto'), {
 rs.fit(iris.data, iris.target)
 rs_result = pd.DataFrame(rs.cv_results_)[['param_C', 'param_kernel', 'mean_test_score']]
 print(rs_result)
+
+""" 使用RandomizedSearchCV研究不同模型的不同参数对准确度的影响 """
+model_params = {
+    'svm': {
+        'model': svm.SVC(gamma='auto'),
+        'params': {
+            'C': [1, 10, 20],
+            'kernel': ['rbf', 'linear']
+        }
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params': {
+            'n_estimators': [1, 5, 10]
+        }
+    },
+    'logistic_regression': {
+        'model': LogisticRegression(solver='liblinear', multi_class='auto'),
+        'params': {
+            'C': [1, 5, 10]
+        }
+    }
+}
+scores = []
+for model_name, mp in model_params.items():
+    clf = GridSearchCV(mp['model'], mp['params'], cv=5, return_train_score=False)
+    clf.fit(iris.data, iris.target)
+    scores.append({
+        'model': model_name,
+        'best_score': clf.best_score_,
+        'best_params': clf.best_params_
+    })
+result_scores = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
+print("研究不同模型的不同参数对准确度的影响:")
+print(result_scores)
